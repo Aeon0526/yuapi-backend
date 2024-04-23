736b8ac0af6c065ada29332b21ef4b5af9591f21
Sun <1176550817@qq.com>
2024-04-23 17:03:22 +0800

    第五次提交
    补充网关业务逻辑
    开发监控统计功能

--- src/main/java/com/yupi/springbootinit/aop/AuthInterceptor.java
+++ src/main/java/com/yupi/springbootinit/aop/AuthInterceptor.java
@@ -1,13 +1,11 @@
 package com.yupi.springbootinit.aop;
 
+import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
 import com.yupi.springbootinit.annotation.AuthCheck;
 import com.yupi.springbootinit.common.ErrorCode;
 import com.yupi.springbootinit.exception.BusinessException;
-import com.yupi.springbootinit.model.entity.User;
-import com.yupi.springbootinit.model.enums.UserRoleEnum;
 import com.yupi.springbootinit.service.UserService;
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
+import com.yupi.yuapicommon.model.entity.User;
 import org.apache.commons.lang3.StringUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -16,6 +14,12 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 权限校验 AOP
@@ -27,43 +31,40 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 @Component
 public class AuthInterceptor {
 
-    @Resource
-    private UserService userService;
+	@Resource
+	private UserService userService;
 
-    /**
-     * 执行拦截
-     *
-     * @param joinPoint
-     * @param authCheck
-     * @return
-     */
-    @Around("@annotation(authCheck)")
-    public Object doInterceptor(ProceedingJoinPoint joinPoint, AuthCheck authCheck) throws Throwable {
-        String mustRole = authCheck.mustRole();
-        RequestAttributes requestAttributes = RequestContextHolder.currentRequestAttributes();
-        HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
-        // 当前登录用户
-        User loginUser = userService.getLoginUser(request);
-        // 必须有该权限才通过
-        if (StringUtils.isNotBlank(mustRole)) {
-            UserRoleEnum mustUserRoleEnum = UserRoleEnum.getEnumByValue(mustRole);
-            if (mustUserRoleEnum == null) {
-                throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
-            }
-            String userRole = loginUser.getUserRole();
-            // 如果被封号，直接拒绝
-            if (UserRoleEnum.BAN.equals(mustUserRoleEnum)) {
-                throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
-            }
-            // 必须有管理员权限
-            if (UserRoleEnum.ADMIN.equals(mustUserRoleEnum)) {
-                if (!mustRole.equals(userRole)) {
-                    throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
-                }
-            }
-        }
-        // 通过权限校验，放行
-        return joinPoint.proceed();
-    }
+	/**
+	 * 执行拦截
+	 *
+	 * @param joinPoint
+	 * @param authCheck
+	 * @return
+	 */
+	@Around("@annotation(authCheck)")
+	public Object doInterceptor(ProceedingJoinPoint joinPoint, AuthCheck authCheck) throws Throwable {
+		List<String> anyRole = Arrays.stream(authCheck.anyRole()).filter(StringUtils::isNotBlank).collect(Collectors.toList());
+		String mustRole = authCheck.mustRole();
+		RequestAttributes requestAttributes = RequestContextHolder.currentRequestAttributes();
+		HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+		// 当前登录用户
+		User user = userService.getLoginUser(request);
+		// 拥有任意权限即通过
+		if (CollectionUtils.isNotEmpty(anyRole)) {
+			String userRole = user.getUserRole();
+			if (!anyRole.contains(userRole)) {
+				throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+			}
+		}
+		// 必须有所有权限才通过
+		if (StringUtils.isNotBlank(mustRole)) {
+			String userRole = user.getUserRole();
+			if (!mustRole.equals(userRole)) {
+				throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+			}
+		}
+		// 通过权限校验，放行
+		return joinPoint.proceed();
+	}
 }
 
